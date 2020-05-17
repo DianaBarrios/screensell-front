f6030fefe98fc778e1670fd54deea978cdbe2f3b
Diana Barrios <dianabarriosmacias@hotmail.com>
2020-05-16 21:40:07 -0700

    products

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 
 class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            products: []
+    };
+    }
+
+    componentWillMount() {
+        fetch("https://screensell-back.herokuapp.com/product/")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({products: data.products}) 
+        })
+    }
+  
     render(){
+        const {products} = this.state;
+
         return(
             <div className="page-division">
                 <Sidebar/>
                 <div className="page-content">
                     <h2>PRODUCTOS</h2>
+                    <div>
+                        <ul>
+                            {
+                                products.map(product =>
+                                    <li>
+                                        <h3>{product.name}</h3>
+                                    </li>
+                                    )
+                            }
+
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
