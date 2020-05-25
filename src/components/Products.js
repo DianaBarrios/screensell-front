7debca16d36cdd7c87d355b8a2e2f30fb843b398
Diana Barrios <dianabarriosmacias@hotmail.com>
2020-05-25 13:31:05 -0700

    added search

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,7 +10,15 @@ class Product extends Component {
       products: [],
       isLoading: false,
       error: null,
+<<<<<<< HEAD
+=======
+      query: "",
+      select: ""
+>>>>>>> added search in products
     };
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+    this.handleSelectInput = this.handleSelectInput.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.handleClickOnProduct = this.handleClickOnProduct.bind(this);
   }
 
@@ -32,6 +40,62 @@ class Product extends Component {
       });
     }
   }
+
+  handleSearchInput(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  handleSelectInput(e){
+    this.setState({ select: e.target.value });
+  }
+
+async handleSearch(e) {
+    e.preventDefault();
+    let option = this.refs.searchOption.value;
+    let q = this.state.query;
+    console.log("query:",q);
+    console.log("option:",option);
+
+    if(option == "nombre"){
+     /* /:name */
+     this.setState({ isLoading: true });
+
+    try {
+      const result = await axios.get(
+        `https://screensell-back.herokuapp.com/product/${q}`
+      );
+      this.setState({
+        products: result.data,
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false
+      });
+    }
+      
+    } else {
+      /* /getid/:id */
+    this.setState({ isLoading: true });
+
+    try {
+      const result = await axios.get(
+        `https://screensell-back.herokuapp.com/product/getid/${q}`
+      );
+      this.setState({
+        products: [result.data],
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false
+      });
+    }
+    
+  }
+}
 
   handleClickOnProduct(id) {
     let newPath = `/producto/${id}`;
@@ -55,15 +119,34 @@ class Product extends Component {
         <div className="page-content mt-3 px-4">
           <h2 className="page-title">PRODUCTOS</h2>
 
-          <div className="row d-flex justify-content-end mx-3 mt-4">
+          <div className="row d-flex justify-content-end mx-3 my-4">
             <a href="/producto/nuevo" className="btn btn-primary">
               Agregar producto
             </a>
           </div>
 
-          <div className="container">
-            <Searchbar />
-          </div>
+          <form id="search-product-form" className="form-inline my-2 my-lg-0">
+            <input
+              id="search-input"
+              type="search"
+              value={this.state.query}
+              onChange={this.handleSearchInput}
+              placeholder="Buscar por nombre o id"
+            />
+            <select 
+              id="search-select"
+              ref="searchOption"
+              value={this.state.select}
+              onChange={this.handleSelectInput} 
+            >
+              <option value="nombre">Nombre</option>
+              <option value="id">ID</option>
+            </select>
+
+            <button id="search-btn" type="submit" onClick={this.handleSearch}>
+              Buscar
+            </button>
+          </form>
 
           <div className="container mt-3">
             <table className="table table-hover px-3">
