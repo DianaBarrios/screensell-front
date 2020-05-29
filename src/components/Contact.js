1d38c6a07beb3644938589014aa0995dc4ed1963
aliciacisnerosm <aliciacisnerosm@gmail.com>
2020-05-29 02:30:27 -0500

    correos de contacto - ya me quiero dormir (:

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,58 @@ import React, { Component } from "react";
 import "./Contact.css";
 import Map from "./Map";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { send } from "q";
+import { couldStartTrivia } from "typescript";
+
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      name: "",
+      email: ""
+    };
+
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChangeName(e) {
+    this.setState({ name: e.target.value });
+  }
+  onChangeMessage(e) {
+    this.setState({ message: e.target.value });
+  }
+  onChangeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let mail = {
+      to: 'aliciacisnerosm@gmail.com',
+      subject: 'Contacto en Screensell',
+      text: `¡Hola!` + `\n Te informamos que ${this.state.name} dejó un comentario para screensell: \n` +
+        this.state.message + " \n Te puedes comunicar con usuario por medio de este correo electrónico: " + this.state.email + "\n ¡Saludos! \n  Alis & Dianix"
+    };
+    console.log(mail);
+    axios
+      .post('https://screensell-back.herokuapp.com/mail', mail)
+      .then((result) => {
+        console.log('Email mandado!');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+
+  }
+
   render() {
+
     return (
       <div className="main">
         <h2>Contacto</h2>
@@ -17,6 +67,7 @@ class Contact extends Component {
                 <form>
                   <div className="form-group">
                     <input
+                      onChange={this.onChangeName}
                       type="text"
                       className="form-control"
                       placeholder="Nombre"
@@ -31,6 +82,7 @@ class Contact extends Component {
                       placeholder="Correo electrónico"
                       id="exampleInputEmail1"
                       aria-describedby="emailHelp"
+                      onChange={this.onChangeEmail}
                       required
                     />
                   </div>
@@ -42,19 +94,17 @@ class Contact extends Component {
                       id="exampleFormControlTextarea1"
                       rows="3"
                       required
+                      onChange={this.onChangeMessage}
                     ></textarea>
                   </div>
 
                   <button
                     className="btn btn-primary send-button"
-                    id="submit"
-                    type="submit"
-                    value="SEND"
+                    onClick={this.onSubmit}
                   >
-                    <a href="mailto:dianabarriosmacias@gmail.com?Subject=Contacto%20Screensell">
-                      <i className="fa fa-paper-plane mr-2"></i>
-                      <span id="send-text">ENVIAR</span>
-                    </a>
+                    <i className="fa fa-paper-plane mr-2"></i>
+                    <span id="send-text">ENVIAR</span>
+
                   </button>
                 </form>
               </div>
